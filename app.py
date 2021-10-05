2ea72e64606b60cacd820bb3f8efa74fd87cd496
brfr82 <91781373+brfr82@users.noreply.github.com>
2021-10-05 23:31:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,109 @@
 
+
+
+
+import pandas as pd
+import networkx
+import matplotlib.pyplot as plt
+import numpy as np
+
+pip install bokeh
+
+
+got_df = pd.read_csv('got-edges.csv')
+
+
+got_df
+
+G = networkx.from_pandas_edgelist(got_df, 'Source', 'Target', 'Weight')
+
+
+from bokeh.io import output_notebook, show, save
+from bokeh.models import Range1d, Circle, ColumnDataSource, MultiLine
+from bokeh.plotting import figure
+from bokeh.plotting import from_networkx
+
+
+#Choose a title!
+title = 'Euro Soccer Transfer market 2019'
+
+#Establish which categories will appear when hovering over each node
+HOVER_TOOLTIPS = [("Character", "@index")]
+
+#Create a plot — set dimensions, toolbar, and title
+plot = figure(tooltips = HOVER_TOOLTIPS,
+              tools="pan,wheel_zoom,save,reset", active_scroll='wheel_zoom',
+            x_range=Range1d(-10.1, 10.1), y_range=Range1d(-10.1, 10.1), title=title)
+
+#Create a network graph object with spring layout
+# https://networkx.github.io/documentation/networkx-1.9/reference/generated/networkx.drawing.layout.spring_layout.html
+network_graph = from_networkx(G, networkx.spring_layout, scale=10, center=(0, 0))
+
+#Set node size and color
+network_graph.node_renderer.glyph = Circle(size=15, fill_color='skyblue')
+
+#Set edge opacity and width
+network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.5, line_width=1)
+
+#Add network graph to the plot
+plot.renderers.append(network_graph)
+
+show(plot)
+#save(plot, filename=f"{title}.html")
+
+
+from bokeh.io import output_notebook, show, save
+from bokeh.models import Range1d, Circle, ColumnDataSource, MultiLine
+from bokeh.plotting import figure
+from bokeh.plotting import from_networkx
+from bokeh.palettes import Blues8, Reds8, Purples8, Oranges8, Viridis8, Spectral8
+from bokeh.transform import linear_cmap
+
+
+degrees = dict(networkx.degree(G))
+networkx.set_node_attributes(G, name='degree', values=degrees)
+
+
+number_to_adjust_by = 5
+adjusted_node_size = dict([(node, degree+number_to_adjust_by) for node, degree in networkx.degree(G)])
+networkx.set_node_attributes(G, name='adjusted_node_size', values=adjusted_node_size)
+
+
+
+#Choose attributes from G network to size and color by — setting manual size (e.g. 10) or color (e.g. 'skyblue') also allowed
+size_by_this_attribute = 'adjusted_node_size'
+color_by_this_attribute = 'adjusted_node_size'
+
+#Pick a color palette — Blues8, Reds8, Purples8, Oranges8, Viridis8
+color_palette = Blues8
+
+#Choose a title!
+title = 'Euro Soccer Market Transfer 2019 Network'
+
+#Establish which categories will appear when hovering over each node
+HOVER_TOOLTIPS = [
+       ("Character", "@index"),
+        ("Degree", "@degree")
+]
+
+#Create a plot — set dimensions, toolbar, and title
+plot = figure(tooltips = HOVER_TOOLTIPS,
+              tools="pan,wheel_zoom,save,reset", active_scroll='wheel_zoom',
+            x_range=Range1d(-10.1, 10.1), y_range=Range1d(-10.1, 10.1), title=title)
+
+#Create a network graph object
+# https://networkx.github.io/documentation/networkx-1.9/reference/generated/networkx.drawing.layout.spring_layout.html\
+network_graph = from_networkx(G, networkx.spring_layout, scale=10, center=(0, 0))
+
+#Set node sizes and colors according to node degree (color as spectrum of color palette)
+minimum_value_color = min(network_graph.node_renderer.data_source.data[color_by_this_attribute])
+maximum_value_color = max(network_graph.node_renderer.data_source.data[color_by_this_attribute])
+network_graph.node_renderer.glyph = Circle(size=size_by_this_attribute, fill_color=linear_cmap(color_by_this_attribute, color_palette, minimum_value_color, maximum_value_color))
+
+#Set edge opacity and width
+network_graph.edge_renderer.glyph = MultiLine(line_alpha=0.5, line_width=1)
+
+plot.renderers.append(network_graph)
+
+show(plot)
+#save(plot, filename=f"{title}.html")
